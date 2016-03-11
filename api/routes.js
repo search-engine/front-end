5d23adce9df641dbe94c4cc3b49636ea9db6d181
ed12al <ed12al@hotmail.com>
2016-03-10 21:59:08 -0800

    abc

--- api/routes.js
+++ api/routes.js
@@ -3,13 +3,10 @@ var mongoose = require('mongoose');
 
 
 mongoose.connect('mongodb://localhost/search_engine', function (error) {
-		console.log('hi');
 		    if (error) {
-
 		        console.log(error);
 		    }
 		});
-
 		var Schema = mongoose.Schema;
 		var userSchema = new Schema({
     		word: String ,
@@ -23,13 +20,13 @@ mongoose.connect('mongodb://localhost/search_engine', function (error) {
 
 module.exports = function(app) {
 	app.get('/api/search/results', function(req, res){
-		//get the result here
-		//res.json(JSON.parse(result.text));
-		console.log(req.query.q);
-		if (req.query.q) {
-        User.find({word:req.query.q}, function (err, docs) {
+		var keys = req.query.q;
+		var keywords = [];
+		if(keys){ keywords = keys.split(" ");}
+		console.log(keywords);
+		if (keywords[0]) {
+        	User.find({word: keywords[0]}, function (err, docs) {
         	console.log(docs);
-
             return (res.json(docs));
         });
     }
