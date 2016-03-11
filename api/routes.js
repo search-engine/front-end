d76547ccdea68b3e8b4003347c06cd3e1b152f7d
ed12al <ed12al@hotmail.com>
2016-03-10 19:12:06 -0800

    bug

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,7 @@
-var superagent = require('superagent');
-
 module.exports = function(app) {
 	app.get('/api/search/results', function(req, res){
 		//get the result here
 		//res.json(JSON.parse(result.text));
-		})
 	});
+
 }
