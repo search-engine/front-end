ab34eb9b3a0056e54c5d0f392682205d0e941705
ed12al <ed12al@hotmail.com>
2016-03-11 14:59:14 -0800

    boolean

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,6 @@
-
 var mongoose = require('mongoose');
-
+var async = require('async');
+var HashMap = require('hashmap');
 
 mongoose.connect('mongodb://localhost/search_engine', function (error) {
 		    if (error) {
@@ -22,16 +22,79 @@ module.exports = function(app) {
 	app.get('/api/search/results', function(req, res){
 		var keys = req.query.q;
 		var keywords = [];
-		if(keys){ keywords = keys.split(" ");}
-		console.log(keywords);
-		if (keywords[0]) {
-        	User.find({word: keywords[0]}, function (err, docs) {
-        	console.log(docs);
-            return (res.json(docs));
-        });
-    }
+		var map = new HashMap();
+		var status = 0;
 
-		})
+		if(keys){ keywords = keys.split(" ");}
+		
+		async.eachSeries(keywords, function (item, callback) {
+			console.log(item);
+			if(item === "AND"){
+				status = 1;
+				console.log("found ANAND")
+				callback();
+			}else if(item === "OR"){
+				status = 2;
+				console.log("found OR")
+				callback();
+			}else{
+				if(status === 0){
+					//normal case
+  					User.findOne({word: item.toLowerCase()}, function (err, docs) {
+  						var ranking = docs.ranks;
+        				for (var i = 0; i < ranking.length; i++) {
+        					if(map.has(ranking[i].url)){
+        						var oldScore = map.get(ranking[i].url);
+        						map.set(ranking[i].url, (ranking[i].score+oldScore));
+        					}else{
+        						map.set(ranking[i].url, ranking[i].score);
+        					}
+    					}
+    					status = 0;
+    					console.log("done");
+    					callback();
+        			});
+  				}else if(status === 1){
+  					//AND case
+  					
+  					User.findOne({word: item.toLowerCase()}, function (err, docs) {
+  						console.log("ANAND here");
+        				var ranking = docs.ranks;
+        				for (var i = 0; i < ranking.length; i++) {
+        					if(map.has(ranking[i].url)){
+        						var oldScore = map.get(ranking[i].url);
+        						map.set(ranking[i].url, (ranking[i].score+oldScore));
+        					}else{
+        						map.set(ranking[i].url, ranking[i].score);
+        					}
+    					}
+    					status = 0;
+    					callback();
+        			});
+  				}else{
+  					//OR case
+  					User.findOne({word: item.toLowerCase()}, function (err, docs) {
+  						console.log("OR here");
+  						
+        				var ranking = docs.ranks;
+        				for (var i = 0; i < ranking.length; i++) {
+        					if(map.has(ranking[i].url)){
+        						var oldScore = map.get(ranking[i].url);
+        						map.set(ranking[i].url, (ranking[i].score > oldScore? ranking[i].score : oldScore));
+        					}else{
+        						map.set(ranking[i].url, ranking[i].score);
+        					}
+    					}
+    					status = 0;
+    					callback();
+        			});
+  				}
+  			}
+		}, function done() {
+ 	 		//console.log("keys", map.keys());
+ 	 		return res.send({keys: map.keys(), values: map.values});
+ 		})
+	})
 
 
 }
