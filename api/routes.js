562928f19dce984e5fcd5d8b276e0d0ce5bd61a9
suresanand89 <suresanand@gmail.com>
2016-03-10 20:02:30 -0800

    none

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,42 @@
-var superagent = require('superagent');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/search_engine', function (error) {
+		    if (error) {
+		        console.log(error);
+		    }
+		});
+
+		var Schema = mongoose.Schema;
+		var UserSchema = new Schema({
+    		word: String,
+    		data: {
+    			score: String,
+    			documents: String
+    		}
+		});
+
+		var User = mongoose.model('User', userSchema);
 
 module.exports = function(app) {
 	app.get('/api/search/results', function(req, res){
 		//get the result here
 		//res.json(JSON.parse(result.text));
+
+		if (req.params.word) {
+        User.find({ word: req.params.word }, function (err, docs) {
+        	console.log(docs);
+
+            return (res.json(docs));
+        });
+    }
+
 		})
+
+
+	
+		
+
+		
 	});
 }
