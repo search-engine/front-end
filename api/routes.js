a385a8e917fc8039b6303dc8e3e43e085ed7b8db
sridharharsha <malavaiah.sridhar@gmail.com>
2016-03-11 16:08:42 -0800

    ad

--- api/routes.js
+++ api/routes.js
@@ -20,6 +20,7 @@ mongoose.connect('mongodb://localhost/search_engine', function (error) {
 		},{ collection: 'index' });
 
 		var User = mongoose.model('index', userSchema);
+		module.exports = User;
 
 module.exports = function(app) {
 	app.get('/api/search/results', function(req, res){
@@ -27,11 +28,11 @@ module.exports = function(app) {
 		//res.json(JSON.parse(result.text));
 		console.log(req.query.q);
 		if (req.query.q) {
-        User.find({word:req.query.q}, function (err, docs) {
-        	console.log(docs);
+        User.find({word:new RegExp('^'+req.query.q, 'i')}, function (err, docs) {
+//        	console.log(docs);
 
             return (res.json(docs));
-        });
+        }).limit(10);
     }
 
 		})
--- client/js/search/search.controller.js
+++ client/js/search/search.controller.js
@@ -1,13 +1,75 @@
-angular.module('search.controller', [])
+
+var searchApp = angular.module('search.controller', [])
 .directive('resultsPreview', function(){
 	return {
 		templateUrl: '../views/results.preview.html'
 	}
 })
 .controller('SearchController', function ($scope, SearchService) {
-	$scope.search = function(){
+
+
+	// $scope.search = function(){
+	// 	SearchService.query({q: $scope.keywords}, function(response){
+	// 		$scope.results = response;
+	// 	});
+	// };
+	$scope.simResults = '';
+	$scope.selectedIndex = -1;
+	$scope.getSimilarTerms = function(){
 		SearchService.query({q: $scope.keywords}, function(response){
-			$scope.results = response;
+			$scope.simResults = response;
+
 		});
-	};
+		$scope.similarTerms = [];
+		var maxTermList = 0;
+		for(var i= 0;i<$scope.simResults.length;i++){
+			$scope.similarTerms.push($scope.simResults[i].word);
+			maxTermList += 1;
+			if(maxTermList === 5){
+				break;
+			}
+		}
+
+		$scope.$watch('selectedIndex',function(val){
+			if(val !== -1){
+				$scope.keywords = $scope.similarTerms[$scope.selectedIndex];
+				$scope.similarTerms = [];
+			}
+		});
+
+		$scope.checkDownKey = function(event){
+			if(event.keyCode === 40){
+				event.preventDefault();
+				if($scope.selectedIndex+1 !== $scope.similarTerms.length){
+					$scope.selectedIndex++;
+				}
+			}else if (event.keyCode === 38) {
+				event.preventDefault();
+				if($scope.selectedIndex-1 !== -1){
+					$scope.selectedIndex--;
+				}
+			}else if (event.keyCode === 13) {
+				event.preventDefault();
+				$scope.similarTerms = [];
+			}
+		}
+
+		$scope.checkUpKey = function(event){
+			if(event.keyCode !== 8 || event.keyCode !== 46){
+				if($scope.keywords == ""){
+					$scope.similarTerms = [];
+				}
+			}
+		}
+
+		$scope.selectedValue = function(index){
+			$scope.keywords = $scope.similarTerms[index];
+			$scope.similarTerms = [];
+		}
+
+
+	}
+
+
+
 });
